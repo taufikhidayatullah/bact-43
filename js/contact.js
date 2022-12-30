8d6b9f6f8df4bc4ef1baa29ac094362867c92a3b
4212ganteng <67536564+4212ganteng@users.noreply.github.com>
2022-12-30 16:04:27 +0700

    Update contact.js
    adding comment

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,21 @@
 function submitData() {
+//   membuat variable name , yang berisi sebuah value dari inputan ('f-name)
   let name = document.getElementById('f-name').value
+//   membuat variable email , yang berisi sebuah value dari inputan ('f-email)
   let email = document.getElementById('f-email').value
   let phone = document.getElementById('f-phone').value
   let subject = document.getElementById('f-subject').value
   let message = document.getElementById('f-message').value
-
+// validasi
+  
+//   jika name nya kosong 
   if (name == '') {
+//     maka munculkan alert ('name Form is required')
     return alert('name Form is required')
   }
+  atau //   jika email nya kosong 
   else if (email == '') {
+//         maka munculkan alert ('email Form is required')
     return alert('Email Form is required')
   }
   else if (phone == '') {
@@ -24,7 +31,12 @@ function submitData() {
   // For sending Mail
   let emailRecipient = "thidayatullah23@gmail.com"
 
+//   membuat elemen html a href
+//   membuat elemen a (anchor)
   let mailTo = document.createElement('a')
+//    pangggil element a , yang di simpan di variable mailto
+  
+//   jadi nya a href =
   mailTo.href = `mailto:${emailRecipient}?subject=${subject}&body=Halo Perkenalkan nama saya ${name}%0D%0A%0D%0A${message},%0D%0Asilahkan hubungi saya di ${phone}%0D%0A%0D%0ATerima kasih.`
   mailTo.click()
 }
